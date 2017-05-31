fe57babcfc1b5d73d2af586eed44afcd53aa6916
iuadget <iuadget@gmail.com>
2017-05-31 21:21:07 +0300

    fix

--- queries.sql
+++ queries.sql
@@ -7,8 +7,9 @@ UPDATE `tasks` SET dt_complete = now() WHERE id = 1;
 /*Запрос добавляет новый проект, но здесь неправильно здесь нужно доп вставить id  пользователя найденного по его email*/
 INSERT INTO projects(name,user_id) VALUES ('Название',1);
 /*Вставить новую задачу*/
-INSERT INTO tasks(user_id, project_id, created, deadline, name) VALUES (1, 1, CURDATE(), CURDATE()+ INTERVAL 3 DAY, 'Какая-то новая задача1');
+/* А можно вообще в sql запросе подставлять данные от пользователя?*/
+INSERT INTO tasks(user_id, project_id, dt_created, dt_deadline, name) VALUES (1, 1, CURDATE(), CURDATE()+ INTERVAL 3 DAY, 'Какая-то новая задача1');
 /*Получить все задачи для завтрашнего дня*/
-SELECT * FROM `tasks` WHERE (deadline = CURDATE() + INTERVAL 1 DAY) AND (dt_complete IS NOT 1);
+SELECT * FROM `tasks` WHERE (dt_deadline = CURDATE() + INTERVAL 1 DAY) AND (dt_complete IS NULL);
 /*обновить название задачи по её идентификатору*/
 UPDATE tasks SET name = 'Новое значение' WHERE id = 1; 
